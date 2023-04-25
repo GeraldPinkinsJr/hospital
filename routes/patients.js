0c300da3454072d9fb8d8c2b7c3fbeb89c0b366f
Gerald Pinkins <geraldpinkinsjr@gmail.com>
2023-04-25 11:28:21 -0400

    added Doctors names

--- routes/patients.js
+++ routes/patients.js
@@ -1,9 +1,23 @@
 var express = require("express");
 var router = express.Router();
-
+const { Patient } = require("../models/patient");
 /* GET users listing. */
-router.get("/", function (req, res, next) {
-  res.send("respond with a resource");
+
+router.get("/", (req, res)=> {
+    Patient.findAll()
+    .then((data)=>res.status(201).send(data))
+    .catch((error)=>res.status(400).send(error))
+});
+
+router.post("/add-patient", (req, res) => {
+   Patient.create({
+        firstName: req.body.firstName,
+        lastName: req.body.lastName,
+        insurance: req.body.insurance,
+
+    })
+    .then((data)=> res.status(201).send(data))
+    .catch((error)=>res.status(400).send(error))
 });
 
 module.exports = router;
